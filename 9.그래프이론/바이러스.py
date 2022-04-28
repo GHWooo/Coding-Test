7864e0896eaad29400f6b2241f7c7d008d7bf513
GuHyeonWoo <ghw9174@gmail.com>
2022-04-28 15:13:15 +0900

    그래프이론_바이러스 풀이

--- 9.그래프이론/바이러스.py
+++ 9.그래프이론/바이러스.py
@@ -1,7 +1,26 @@
 import sys
 
-input = sys.stdin.readline()
+input = sys.stdin.readline
 
-A = input
-B = input
-print(A, B)
+A = int(input())
+B = int(input())
+
+comList = [0 for _ in range(101)]
+matchList = []
+
+for _ in range(B):
+    matchList.append(list(map(int, input().split())))
+
+def Graph(t):
+    for i in matchList:
+        if i[0] == t:
+            if comList[i[1]] == 0:
+                comList[i[1]] = 1
+                Graph(i[1])
+        elif i[1] == t:
+            if comList[i[0]] == 0:
+                comList[i[0]] = 1
+                Graph(i[0])
+
+Graph(1)
+print(sum(comList)-1)
