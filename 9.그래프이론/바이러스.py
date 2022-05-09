b6107c288c951fccd6f2745324a18965cec77e16
GuHyeonWoo <ghw9174@gmail.com>
2022-05-09 20:04:43 +0900

    그래프 이론 해설 주석 처리

--- 9.그래프이론/바이러스.py
+++ 9.그래프이론/바이러스.py
@@ -2,25 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
+# 컴퓨터의 개수와 연결된 쌍의 수 입력
 A = int(input())
 B = int(input())
 
+# 확인했음을 표시할 리스트 생성
 comList = [0 for _ in range(101)]
+# 쌍을 입력할 리스트 생성
 matchList = []
 
+# 쌍을 matchList에 삽입
 for _ in range(B):
     matchList.append(list(map(int, input().split())))
 
+# 연결되어있는 컴퓨터의 수를 확인할 함수
 def Graph(t):
+    # matchList안에 있는 쌍을 전부 확인한다.
     for i in matchList:
+        # 쌍의 원소 둘 중 하나라도 t와 같다면
         if i[0] == t:
+            # 남은 원소를 확인 했는지 확인한다.
+            # 확인을 아직 하지 않았다면
             if comList[i[1]] == 0:
+                # 확인했다는 표시를 하고
                 comList[i[1]] = 1
+                # 해당 원소를 다시 재귀함수를 통해 실행한다.
                 Graph(i[1])
         elif i[1] == t:
             if comList[i[0]] == 0:
                 comList[i[0]] = 1
                 Graph(i[0])
 
+# 무조건 1부터 시작하기 때문에 1을 넣어준다.
 Graph(1)
+# 첫번째 컴퓨터는 세지 않기 때문에 1을 뺀 합의 개수를 출력한다.
 print(sum(comList)-1)
--- 9.그래프이론/연결요소의개수.py
+++ 9.그래프이론/연결요소의개수.py
@@ -1,26 +1,36 @@
 import sys
+# 재귀의 깊이를 설정해 주지 않으면 런타임 에러 발생
 sys.setrecursionlimit(10000)
 
 input = sys.stdin.readline
 
+# 정점의 개수와 간선의 개수 입력
 N, M = map(int, input().split())
 
+# 해당 정점을 확인 했음을 표시하는 리스트
 visited = [0 for _ in range(N+1)]
+# 간선으로 연결되어있는 상태를 표시할 리스트
 component = [[] for _ in range(N+1)]
 
 for _ in range(M):
     a, b = map(int, input().split())
+    # 양방향으로 확인해야 하기 때문에 양쪽 다 연결된 요소를 넣어준다.
     component[a].append(b)
     component[b].append(a)
 
+# 연결되어있는 요소를 재귀함수를 통해 확인 표시를 한다.
 def Graph(t):
+    # 확인 표시를 한 후
     visited[t] = 1
+    # 연결되어 있는 요소가
     for i in component[t]:
+        # 아직 확인하지 않았다면 재귀함수 실행
         if visited[i] == 0:
             Graph(i)
     
 result = 0
 
+# 1부터 반복하며 확인하지 않았다면 결과에 1을 더하고 함수 실행
 for i in range(1, N+1, 1):
     if visited[i] == 0:
         result += 1
