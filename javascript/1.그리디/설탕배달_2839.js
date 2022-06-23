0693d4d58648406d7ee41829f2a717d5dba31a85
GuHyeonWoo <ghw9174@gmail.com>
2022-06-23 16:20:39 +0900

    readline -> fs 모듈 변경

--- javascript/1.그리디/설탕배달_2839.js
+++ javascript/1.그리디/설탕배달_2839.js
@@ -1,34 +1,56 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-rl.on("line", function(line){
-    greedy(line);
-    rl.close();
-});
+// rl.on("line", function(line){
+//     greedy(line);
+//     rl.close();
+// });
 
-function greedy(N){
-    let total_count = Number(N);
-    let count_three = 0;
+// function greedy(N){
+//     let total_count = Number(N);
+//     let count_three = 0;
 
-    while(1){
-        if(total_count % 5 === 0){
-            console.log(total_count / 5 + count_three);
-            break;
-        }
+//     while(1){
+//         if(total_count % 5 === 0){
+//             console.log(total_count / 5 + count_three);
+//             break;
+//         }
 
-        total_count -= 3;
-        count_three++;
-        if (total_count < 3 && total_count === 0){
-            console.log(count_three);
-            break
-        }
-        else if(total_count < 3 && total_count !== 0){
-            console.log(-1);
-            break
-        }
+//         total_count -= 3;
+//         count_three++;
+//         if (total_count < 3 && total_count === 0){
+//             console.log(count_three);
+//             break
+//         }
+//         else if(total_count < 3 && total_count !== 0){
+//             console.log(-1);
+//             break
+//         }
+//     }
+// }
+
+const fs = require('fs');
+let N = fs.readFileSync('/dev/stdin').toString().trim();
+let count_three = 0;
+
+while(1){
+    if(N % 5 === 0){
+        console.log(N / 5 + count_three);
+        break;
+    }
+
+    N -= 3;
+    count_three++;
+    if (N < 3 && N === 0){
+        console.log(count_three);
+        break
+    }
+    else if(N < 3 && N !== 0){
+        console.log(-1);
+        break
     }
 }
