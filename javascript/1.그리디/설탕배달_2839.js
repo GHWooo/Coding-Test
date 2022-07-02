754f0dbf6fc37777cc788f4ccdcb27b726f80255
GuHyeonWoo <ghw9174@gmail.com>
2022-07-03 08:30:30 +0900

    불필요한 주석 제거

--- javascript/1.그리디/설탕배달_2839.js
+++ javascript/1.그리디/설탕배달_2839.js
@@ -1,38 +1,3 @@
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
-
-// rl.on("line", function(line){
-//     greedy(line);
-//     rl.close();
-// });
-
-// function greedy(N){
-//     let total_count = Number(N);
-//     let count_three = 0;
-
-//     while(1){
-//         if(total_count % 5 === 0){
-//             console.log(total_count / 5 + count_three);
-//             break;
-//         }
-
-//         total_count -= 3;
-//         count_three++;
-//         if (total_count < 3 && total_count === 0){
-//             console.log(count_three);
-//             break
-//         }
-//         else if(total_count < 3 && total_count !== 0){
-//             console.log(-1);
-//             break
-//         }
-//     }
-// }
-
 const fs = require('fs');
 let N = fs.readFileSync('/dev/stdin').toString().trim();
 let count_three = 0;
