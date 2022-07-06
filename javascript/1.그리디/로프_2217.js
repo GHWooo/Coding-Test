0f588bf04fe316d049216736ec2c16c47d3a7119
GuHyeonWoo <ghw9174@gmail.com>
2022-07-06 23:47:00 +0900

    로프  풀이

---
+++ javascript/1.그리디/로프_2217.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+[n, ...arr] = input;
+n = Number(n);
+arr = arr.map((i)=>Number(i));
+arr.sort(function(a, b){
+    return b - a
+});
+
+const result = [];
+
+for(let i = 0; i < n; i++){
+    result.push(arr[i] * (i+1));
+}
+
+console.log(Math.max(...result));
