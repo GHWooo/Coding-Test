a2ee9848b198d6bdf00c3fa1c0a7b922c6a2945a
GuHyeonWoo <ghw9174@gmail.com>
2022-07-06 23:46:49 +0900

    동전 풀이

---
+++ javascript/1.그리디/동전0_11047.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+[n, ...coin] = input;
+const N = Number(n.split(' ')[0]);
+let K = Number(n.split(' ')[1]);
+
+let result = 0;
+
+for(let i = N - 1; i > -1; i--){
+    if(Number(coin[i]) <= K){
+        result += parseInt(K / Number(coin[i]));
+        K = K % Number(coin[i]);
+
+        if(K === 0){
+            break;
+        }
+    }
+}
+
+console.log(result);
