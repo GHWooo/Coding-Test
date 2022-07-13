f1fce732f3e70ab3b270330d530fd8422eb3f629
GuHyeonWoo <ghw9174@gmail.com>
2022-07-13 21:51:40 +0900

    잃어버린 괄호 풀이

---
+++ javascript/1.그리디/잃어버린괄호_1541.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+
+let Num = '';
+let positive = 0;
+let negative = 0;
+let positive_end = 0;
+
+for(let i = 0; i < input.length; i++){
+    if(input[i] !== '+' && input[i] !== '-'){
+        Num += input[i];
+
+        if(i === input.length - 1){
+            if(positive_end === 0){
+                positive += Number(Num);
+            }
+            else{
+                negative += Number(Num);
+            }
+        }
+    }
+    else{
+        if(positive_end === 0){
+            positive += Number(Num);
+            Num = ''
+        }
+        else{
+            negative += Number(Num);
+            Num = ''
+        }
+
+        if(input[i] === '-' && positive_end === 0){
+            positive_end = 1;
+        }
+    }
+}
+
+console.log(positive - negative);
