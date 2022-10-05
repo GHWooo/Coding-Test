f63de1abe96cabba64936a6f624bb9dcaad48d92
GuHyeonWoo <ghw9174@gmail.com>
2022-10-05 21:03:24 +0900

    upt data_structure code

--- data_structure/heap/MaxHeap.js
+++ data_structure/heap/MaxHeap.js
@@ -1,71 +1,67 @@
-class MaxHeap{
+class MaxHeap {
     constructor() {
-        this.items = [];
+        this.heap = [];
     }
-    swap(index1, index2){
-        const temp = this.items[index1];
-        this.items[index1] = this.items[index2];
-        this.items[index2] = temp;
-    }
-    parentIndex(index){ return Math.floor((index - 1) / 2); }
-    leftChildIndex(index){ return index * 2 + 1; }
-    rightChildIndex(index){ return index * 2 + 2; }
-    parent(index){ return this.items[this.parentIndex(index)]; }
-    leftChild(index){ return this.items[this.leftChildIndex(index)]; }
-    rightChild(index){ return this.items[this.rightChildIndex(index)]; }
-    peek(){ return this.items[0]; }
-    size(){ return this.items.length; }
 
-    // 마지막 인덱스의 값을 올리면서 정렬하는 메서드
-    bubbleUp(){
-        // 가장 마지막 인덱스를 저장한다.
-        let index = this.items.length - 1;
-        // 현재 인덱스의 부모 노드가 존재하고, 부모 노드 값보다 현재 인덱스의 값이 클 동안 반복한다.
-        while(this.parent(index) !== undefined && this.parent(index) < this.items[index]){
-            // 현재 인덱스의 값과 부모 인덱스의 값을 교체한다.
-            this.swap(index, this.parentIndex(index));
-            // 현재 인덱스를 부모 인덱스로 수정한다.
-            index = this.parentIndex(index);
-        }
+    swap(arr, x, y) {
+        let temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+        return;
     }
-    // 최상단 인덱스의 값을 내리면서 정렬하는 메서드
-    bubbleDown(){
-        // 최상단 인덱스를 저장한다.
-        let index = 0;
-        // 현재 인덱스의 왼쪽 자식 노드가 존재하고, 왼쪽 자식 노드의 값과 오른쪽 자식 노드의 값이 현재 인덱스의 값보다 클 동안 반복한다.
-        while(this.leftChild(index) != undefined && 
-        (this.leftChild(index) > this.items[index] || this.rightChild(index > this.items[index]))){
-            // 우선 왼쪽 자식 인덱스를 저장한다.
-            let largerIndex = this.leftChildIndex(index);
-            // 오른쪽 자식 노드 값이 존재하고 오른쪽 자식 노드의 값이 왼쪽 자식 노드의 값보다 크다면
-            if(this.rightChild(index) !== undefined && this.rightChild(index) > this.items[largerIndex]){
-                // 오른쪽 자식 인덱스로 수정한다.
-                largerIndex = this.rightChildIndex(index);
-            }
-            // 현재 인덱스의 값과 저장했던 자식 인덱스의 값을 교체한다.
-            this.swap(largerIndex, index);
-            // 현재 인덱스를 저장했던 자식 인덱스로 수정한다.
-            index = largerIndex;
+
+    empty() {
+        if (this.heap.length == 0) {
+            return true;
         }
+        return false;
     }
-    // 힙에 원소를 추가하는 메서드
-    add(item){
-        // 가장 마지막 인덱스 뒤에 원소를 추가한다.
-        this.items[this.items.length] = item;
-        // 추가된 원소를 올리면서 정렬한다.
+
+    insert(value) {
+        this.heap.push(value);
         this.bubbleUp();
     }
-    // 힙에서 최상단 원소를 빼내는 메서드
-    poll(){
-        // 최상단 원소를 따로 저장한다.
-        let item = this.items[0];
-        // 가장 마지막 인덱스의 원소를 첫번째 원소로 복사한다.
-        this.items[0] = this.items[this.items.length - 1];
-        // 가장 마지막 인덱스의 원소를 삭제한다.
-        this.items.pop();
-        // 복사된 최상단 원소를 내리면서 정렬한다.
-        this.bubbleDown();
-        // 저장해둔 원소를 반환한다.
-        return item;
+
+    bubbleUp() {
+        let currentIndex = this.heap.length - 1;
+
+        while (currentIndex > 0) {
+            const parentIndex = Math.floor((currentIndex - 1) / 2);
+
+            if (this.heap[parentIndex] >= this.heap[currentIndex]) break;
+            this.swap(this.heap, parentIndex, currentIndex);
+            currentIndex = parentIndex;
+        }
+    }
+
+    extractMax() {
+        if (this.heap.length == 1) {
+            return this.heap.pop();
+        }
+        const max = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown(0);
+
+        return max;
+    }
+
+    bubbleDown(index) {
+        const leftIndex = 2 * index + 1;
+        const rightIndex = 2 * index + 2;
+        const length = this.heap.length;
+        let largestIndex = index;
+
+        if (leftIndex < length && this.heap[leftIndex] > this.heap[largestIndex]) {
+            largestIndex = leftIndex;
+        }
+
+        if (rightIndex < length && this.heap[rightIndex] > this.heap[largestIndex]) {
+            largestIndex = rightIndex;
+        }
+
+        if (largestIndex !== index) {
+            this.swap(this.heap, largestIndex, index);
+            this.bubbleDown(largestIndex);
+        }
     }
 }
--- data_structure/heap/MinHeap.js
+++ data_structure/heap/MinHeap.js
@@ -1,71 +1,64 @@
-class MinHeap{
+class MinHeap {
     constructor() {
-        this.items = [];
+      this.heap = [];
     }
-    swap(index1, index2){
-        const temp = this.items[index1];
-        this.items[index1] = this.items[index2];
-        this.items[index2] = temp;
+  
+    empty() {
+      if (this.heap.length == 0) {
+        return true;
+      }
+      return false;
     }
-    parentIndex(index){ return Math.floor((index - 1) / 2); }
-    leftChildIndex(index){ return index * 2 + 1; }
-    rightChildIndex(index){ return index * 2 + 2; }
-    parent(index){ return this.items[this.parentIndex(index)]; }
-    leftChild(index){ return this.items[this.leftChildIndex(index)]; }
-    rightChild(index){ return this.items[this.rightChildIndex(index)]; }
-    peek(){ return this.items[0]; }
-    size(){ return this.items.length; }
+  
+    swap(arr, x, y) {
+      let temp = arr[x];
+      arr[x] = arr[y];
+      arr[y] = temp;
+      return;
+    }
+  
+  
+    insert(value) {
+      this.heap.push(value);
+      this.bubbleUp();
+    }
+  
+    bubbleUp() {
+      let currentIndex = this.heap.length - 1;
+  
+      while (currentIndex > 0) {
+        const parentIndex = Math.floor((currentIndex - 1) / 2);
+        if (this.heap[parentIndex] <= this.heap[currentIndex]) break;
+        this.swap(this.heap, parentIndex, currentIndex)
+        currentIndex = parentIndex;
+      }
+    }
+  
+    extractMin() {
+      if (this.heap.length == 1) {
+        return this.heap.pop();
+      }
+      const min = this.heap[0];
+      this.heap[0] = this.heap.pop();
+      this.bubbleDown(0);
+      return min
+    }
 
-    // 마지막 인덱스의 값을 올리면서 정렬하는 메서드
-    bubbleUp(){
-        // 가장 마지막 인덱스를 저장한다.
-        let index = this.items.length - 1;
-        // 현재 인덱스의 부모 노드가 존재하고, 부모 노드 값보다 현재 인덱스의 값이 작을 동안 반복한다.
-        while(this.parent(index) !== undefined && this.parent(index) > this.items[index]){
-            // 현재 인덱스의 값과 부모 인덱스의 값을 교체한다.
-            this.swap(index, this.parentIndex(index));
-            // 현재 인덱스를 부모 인덱스로 수정한다.
-            index = this.parentIndex(index);
-        }
+    bubbleDown(index) {
+      const leftIndex = 2 * index + 1;
+      const rightIndex = 2 * index + 2;
+      const length = this.heap.length;
+      let smallestIndex = index;
+  
+      if (leftIndex < length && this.heap[leftIndex] < this.heap[smallestIndex]) {
+        smallestIndex = leftIndex;
+      }
+      if (rightIndex < length && this.heap[rightIndex] < this.heap[smallestIndex]) {
+        smallestIndex = rightIndex;
+      }
+      if (smallestIndex !== index) {
+        this.swap(this.heap, index, smallestIndex);
+        this.bubbleDown(smallestIndex)
+      }
     }
-    // 최상단 인덱스의 값을 내리면서 정렬하는 메서드
-    bubbleDown(){
-        // 최상단 인덱스를 저장한다.
-        let index = 0;
-        // 현재 인덱스의 왼쪽 자식 노드가 존재하고, 왼쪽 자식 노드의 값과 오른쪽 자식 노드의 값이 현재 인덱스의 값보다 작을 동안 반복한다.
-        while(this.leftChild(index) != undefined && 
-        (this.leftChild(index) < this.items[index] || this.rightChild(index < this.items[index]))){
-            // 우선 왼쪽 자식 인덱스를 저장한다.
-            let smallerIndex = this.leftChildIndex(index);
-            // 오른쪽 자식 노드 값이 존재하고 오른쪽 자식 노드의 값이 왼쪽 자식 노드의 값보다 작다면
-            if(this.rightChild(index) !== undefined && this.rightChild(index) < this.items[smallerIndex]){
-                // 오른쪽 자식 인덱스로 수정한다.
-                smallerIndex = this.rightChildIndex(index);
-            }
-            // 현재 인덱스의 값과 저장했던 자식 인덱스의 값을 교체한다.
-            this.swap(index, smallerIndex);
-            // 현재 인덱스를 저장했던 자식 인덱스로 수정한다.
-            index = smallerIndex;
-        }
-    }
-    // 힙에 원소를 추가하는 메서드
-    add(item){
-        // 가장 마지막 인덱스 뒤에 원소를 추가한다.
-        this.items[this.items.length] = item;
-        // 추가된 원소를 올리면서 정렬한다.
-        this.bubbleUp();
-    }
-    // 힙에서 최상단 원소를 빼내는 메서드
-    poll(){
-        // 최상단 원소를 따로 저장한다.
-        let item = this.items[0];
-        // 가장 마지막 인덱스의 원소를 첫번째 원소로 복사한다.
-        this.items[0] = this.items[this.items.length - 1];
-        // 가장 마지막 인덱스의 원소를 삭제한다.
-        this.items.pop();
-        // 복사된 최상단 원소를 내리면서 정렬한다.
-        this.bubbleDown();
-        // 저장해둔 원소를 반환한다.
-        return item;
-    }
-}
+  }
