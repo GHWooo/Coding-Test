1e73778bdb948b5cd44bebb3483d61208459ddd7
구현우 <guhyeon-u@guhyeon-uui-MacBookAir.local>
2023-01-10 21:34:13 +0900

    solved 2058

--- java/swea/Solution_2058.java
+++ java/swea/Solution_2058.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Solution_2058 {
+// solved
+import java.util.Scanner;
+
+public class Solution_2058 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String T = sc.nextLine();
+        String[] number_list = T.split("");
+
+        int sum = 0;
+
+        for(int i = 0; i < number_list.length; i++) {
+            int number = Integer.parseInt(number_list[i]);
+            sum += number;
+        }
+
+        System.out.println(sum);
+    }
 }
