7ea01e4e86781fd82788d43f415c8d5f14ea47ee
구현우 <guhyeon-u@guhyeon-uui-MacBookAir.local>
2023-01-10 21:31:25 +0900

    solved 2068

--- java/swea/Solution_2068.java
+++ java/swea/Solution_2068.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Solution_2068 {
+// solved
+import java.util.Scanner;
+
+public class Solution_2068 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int result = 0;
+
+            for(int i = 0; i < 10; i++) {
+                int number = sc.nextInt();
+                if(result < number) result = number;
+            }
+
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
 }
