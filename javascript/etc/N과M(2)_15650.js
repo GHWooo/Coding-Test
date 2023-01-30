13002cfaa467845e2eec642aa2460356c616257a
구현우 <ghw9174@gmail.com>
2023-01-30 22:02:50 +0900

    solved 15650

---
+++ javascript/etc/N과M(2)_15650.js
@@ -0,0 +1,19 @@
+// solved
+
+const fs = require('fs');
+const [n, m] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+const result = [];
+
+const add = (number, str, count) => {
+	if(count === m) {
+		result.push(str);
+		return;
+	}
+	
+	for(let i = number + 1; i <= n; i++) add(i, str + `${i}`, count + 1);
+}
+
+for(let i = 1; i <= (n - m + 1); i++) add(i, `${i}`, 1);
+
+console.log(result.map(str => str.split('').join(' ')).join('\n'));
